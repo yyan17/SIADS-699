ea1b04df03e120ccc4bb4d92c0abb48382a1c154
Sonia Yan <soniayan@umich.com>
2023-06-20 05:17:29 -0400

    update data with tech indicators

--- scripts/get_market_data.py
+++ scripts/get_market_data.py
@@ -1,15 +1,31 @@
+import pandas as pd
 import yfinance as yf
+from ta import add_all_ta_features
+from ta.utils import dropna
 
 
 def download_market_data(tickers: list) -> None:
     for ticker in tickers:
         md = yf.download(ticker, start="2008-06-02", end="2023-05-30")
-        md.to_csv(f"{ticker}.csv")
+        market_data_df = get_technical_indicators(md)
+        market_data_df.to_csv(f"{ticker}.csv")
+
+
+def get_technical_indicators(df: pd.DataFrame) -> pd.DataFrame:
+    # Clean NaN values
+    df = dropna(df)
+
+    # Add all ta features
+    df_with_technical_indicators = add_all_ta_features(
+        df, open="Open", high="High", low="Low", close="Close", volume="Volume")
+
+    return df_with_technical_indicators
 
 
 if __name__ == "__main__":
-    tickers = ['^GSPC', 'CL=F', 'HSI', 'SHA', '^STI', '^BSESN', 'TATAMOTORS.NS', 'SBIN.NS', 'IOC.NS', 'RAJESHEXPO.NS']
+    tickers = ['^NSEI', '^GSPC', 'CL=F', 'HSI', 'SHA', '^STI', '^BSESN', 'TATAMOTORS.NS', 'SBIN.NS', 'IOC.NS', 'RAJESHEXPO.NS']
     download_market_data(tickers)
+
 
 
 
