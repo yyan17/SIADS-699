7d0da69df97f5a2a90adf230c0fa39718ffad177
Sonia Yan <soniayan@umich.com>
2023-07-03 11:00:21 -0400

    add normalized data

--- scripts/get_market_data.py
+++ scripts/get_market_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import yfinance as yf
+from sklearn.preprocessing import MinMaxScaler
 from ta import add_all_ta_features
 from ta.utils import dropna
 
@@ -8,7 +9,8 @@ def download_market_data(tickers: list) -> None:
     for ticker in tickers:
         md = yf.download(ticker, start="2008-06-02", end="2023-05-30")
         market_data_df = get_technical_indicators(md)
-        market_data_df.to_csv(f"{ticker}.csv")
+        updated_df = data_preprocessing(market_data_df)
+        updated_df.to_csv(f"{ticker}.csv")
 
 
 def get_technical_indicators(df: pd.DataFrame) -> pd.DataFrame:
@@ -22,10 +24,22 @@ def get_technical_indicators(df: pd.DataFrame) -> pd.DataFrame:
     return df_with_technical_indicators
 
 
+def data_preprocessing(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.dropna(axis=1, how='any')
+
+    scaler = MinMaxScaler()
+    df[df.columns] = scaler.fit_transform(df[df.columns])
+
+    return df
+
+
 if __name__ == "__main__":
     tickers = ['^NSEI', '^GSPC', 'CL=F', 'HSI', 'SHA', '^STI', '^BSESN', 'TATAMOTORS.NS', 'SBIN.NS', 'IOC.NS',
-               'RAJESHEXPO.NS', 'INR=X']
+               'RAJESHEXPO.NS'] #'INR=X'
     download_market_data(tickers)
+
+
+
 
 
 
