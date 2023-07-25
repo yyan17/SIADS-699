8d5a005e2e50b5d25109501cb05c11085c99d693
Sonia Yan <soniayan@umich.com>
2023-07-25 00:22:53 -0400

    add date column

--- scripts/get_uncompressed_csv_file.py
+++ scripts/get_uncompressed_csv_file.py
@@ -5,21 +5,30 @@ def main(target_file, shap_file, feature_file):
     # Load the shap values
     shap_values = pd.read_csv(shap_file)
 
-    # Get the top 50 features
-    top_53_features = shap_values.head(53)['feature'].tolist()
+    # Get the top 53 features
+    top_50_features = shap_values.head(53)['feature'].tolist()
+
+    # Ensure 'Date' is always included
+    if 'date' not in top_50_features:
+        top_50_features.append('date')
 
     # Load the features data
-    features_data = pd.read_csv(feature_file, compression='gzip')
+    features_data = pd.read_csv(feature_file)
 
-    # Select the top 50 features from the features data
-    features_data_top_50 = features_data[top_53_features]
+    # Select the top 53 features from the features data
+    features_data_top_50 = features_data[top_50_features]
 
-    # Save the top 50 features data
+    # Make 'Date' the first column
+    columns = features_data_top_50.columns.tolist()
+    columns.insert(0, columns.pop(columns.index('date')))
+    features_data_top_50 = features_data_top_50.reindex(columns=columns)
+
+    # Save the top 53 features data
     features_data_top_50.to_csv(target_file, index=False)
 
 
 # Specify the directory where your .csv.gz files are located
 shap_file = r'C:\Users\yangy\Desktop\workspace\SIADS-699\datasets\processed_data\feature_importance\TTML.NS_shap_values.csv'
-feature_file = r'C:\Users\yangy\Desktop\workspace\SIADS-699\datasets\processed_data\combined_features\TTML.NS_features.csv.gz'
+feature_file = r'C:\Users\yangy\Desktop\workspace\SIADS-699\datasets\processed_data\combined_features\TTML.NS_combined.csv'
 target_file = r"C:\Users\yangy\Desktop\workspace\SIADS-699\datasets\processed_data\combined_features\TTML.NS_features_top_53_with_sentiment.csv"
 main(target_file, shap_file, feature_file)
